8adb24593045e6104e7868e6e3eae4a088829e0d
Purnendu Chakraborty <purnendu.chakraborty@nasa.gov>
2018-05-10 12:16:11 -0400

    Added functor EqlbDist to evaluate eqlb dist at a lattice node

---
+++ src/EqlbDist.hpp
@@ -0,0 +1,31 @@
+#ifndef EQLBDIST_HPP
+#define EQLBDIST_HPP
+
+#include <array>
+#include <vector>
+#include "LBModel.hpp"
+
+class EqlbDist{
+public:
+    EqlbDist(){}
+    __attribute__((always_inline))
+    inline void operator()(
+        const LBModel *lbmodel,
+        const float rholocal,
+        const std::array<float, 3> &ulocal,
+        std::vector<float> &nlocal
+    ){
+        const auto kdim = lbmodel->getNumberOfDirections();
+        const auto w = lbmodel->getDirectionalWeights();
+        const auto c = lbmodel->getLatticeVelocities();
+        auto usq = ulocal[0]*ulocal[0] + ulocal[1]*ulocal[1] + ulocal[2]*ulocal[2];
+        assert(nlocal.size()==kdim);
+        for (auto k=0; k<kdim; ++k){
+            auto ck = &c[k*3];
+            auto cu = ck[0]*ulocal[0] + ck[1]*ulocal[1] + ck[2]*ulocal[2];
+            nlocal[k] = w[k]*rholocal*(1.0+3.0*cu+4.5*cu*cu-1.5*usq);
+        }
+    }
+};
+
+#endif
