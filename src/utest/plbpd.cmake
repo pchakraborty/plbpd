3753d3d28628a9a6690e2f3b832b2a2c2c4f922c
Purnendu Chakraborty <purnendu.chakraborty@nasa.gov>
2018-05-15 12:11:51 -0400

    Revert "Running plbpd, but not comparing the output"
    This reverts commit 262c0283be67d0db94f28059bf541bc882ac390f.

--- src/utest/plbpd.cmake
+++ src/utest/plbpd.cmake
@@ -7,11 +7,11 @@ if(NOT "${plbpd_retcode}" STREQUAL "0")
   message(FATAL_ERROR "plbpd failed")
 endif()
 
-# # Compare plbpd output with baseline output
-# execute_process(
-#   COMMAND $ENV{HDF5_DIR}/../../bin/h5diff FinalState.h5 ${CMAKE_CURRENT_LIST_DIR}/FinalState.h5
-#   RESULT_VARIABLE cmp_retcode
-#   )
-# if(NOT "${cmp_retcode}" STREQUAL "0")
-#   message(FATAL_ERROR "comparing FinalState.h5 failed")
-# endif()
+# Compare plbpd output with baseline output
+execute_process(
+  COMMAND $ENV{HDF5_DIR}/../../bin/h5diff FinalState.h5 ${CMAKE_CURRENT_LIST_DIR}/FinalState.h5
+  RESULT_VARIABLE cmp_retcode
+  )
+if(NOT "${cmp_retcode}" STREQUAL "0")
+  message(FATAL_ERROR "comparing FinalState.h5 failed")
+endif()
