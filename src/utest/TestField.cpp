dfe79e82c263b192b67a6acba278e657ffe81579
Purnendu Chakraborty <purnendu.chakraborty@nasa.gov>
2018-06-12 16:25:38 -0400

    Separate function to test Field extents

--- src/utest/TestField.cpp
+++ src/utest/TestField.cpp
@@ -5,94 +5,92 @@
 #include <vector>
 #include <memory>
 
+void assert_extents(uint32_t zsize, uint32_t ysize, uint32_t xsize,
+                    uint32_t nlayers, const Field::FieldExtents& e){
+    assert(e.zbegin == 0+nlayers); assert(e.zend == zsize+nlayers);
+    assert(e.ybegin == 0+nlayers); assert(e.yend == ysize+nlayers);
+    assert(e.xbegin == 0+nlayers); assert(e.xend == xsize+nlayers);
+}
+
+void test_scalar_field_with_0_buffer_layers(){
+    auto zsize = 4;
+    auto ysize = 5;
+    auto xsize = 7;
+
+    // Scalar field
+    Field::Field<float,0> sf(zsize, ysize, xsize, 0);
+    assert(sf.get_dimensions() == std::tie(zsize, ysize, xsize));
+    assert(sf.get_vector_length() == 1);
+
+    // Set data using iterator
+    auto ctr = 0;
+    for (auto it=sf.begin(); it!=sf.end(); ++it)
+        *it = ctr++;
+
+    // Get data using at(z,y,x)
+    Field::FieldExtents e = sf.get_extents();
+    assert_extents(zsize, ysize, xsize, 0, e);
+    for (auto zl=e.zbegin; zl<e.zend; ++zl)
+        for (auto yl=e.ybegin; yl<e.yend; ++yl)
+            for (auto xl=e.xbegin; xl<e.xend; ++xl)
+                assert(sf.at(zl,yl,xl) == sf.get_index(zl,yl,xl));
+}
+
+void test_vector_field_with_0_buffer_layers(){
+    // Vector field with 0 buffer layers
+
+    const auto zsize = 4;
+    const auto ysize = 5;
+    const auto xsize = 7;
+    const auto vsize = 2;
+
+    Field::Field<float,0> vf(zsize, ysize, xsize, vsize, 0.0f);
+    assert(vf.get_dimensions() == std::tie(zsize, ysize, xsize));
+    assert(vf.get_vector_length() == vsize);
+    // Set data using iterator
+    auto ctr = 0;
+    for (auto it=vf.begin(); it!=vf.end(); ++it)
+        *it = ctr++;
+
+    // Get data using at(z,y,x)
+    Field::FieldExtents e = vf.get_extents();
+    assert_extents(zsize, ysize, xsize, 0, e);
+    auto vlen = vf.get_vector_length();
+    for (auto zl=e.zbegin; zl<e.zend; ++zl)
+        for (auto yl=e.ybegin; yl<e.yend; ++yl)
+            for (auto xl=e.xbegin; xl<e.xend; ++xl){
+                auto fndx = vf.get_index(zl,yl,xl,0); // field index
+                for (auto kl=0; kl<vsize; ++kl)
+                    assert(vf.at(zl,yl,xl,kl) == kl+fndx);
+            }
+}
+
+void test_scalar_field_with_1_buffer_layers(){
+    // Vector field with 1 buffer layers
+    const auto zsize=2;
+    const auto ysize=2;
+    const auto xsize=3;
+
+    // Scalar field with one buffer layer
+    auto sf = std::make_unique<Field::Field<float,1> >(zsize, ysize, xsize, 0.0f);
+    size_t zdim, ydim, xdim;
+    std::tie(zdim, ydim, xdim) = sf->get_dimensions();
+    assert(zdim == zsize+2); assert(ydim == ysize+2); assert(xdim == xsize+2);
+    Field::FieldExtents e = sf->get_extents();
+    assert_extents(zsize, ysize, xsize, 1, e);
+    for (auto zl=e.zbegin; zl<e.zend; ++zl)
+        for (auto yl=e.ybegin; yl<e.yend; ++yl)
+            for (auto xl=e.xbegin; xl<e.xend; ++xl)
+                sf->at(zl,yl,xl) = static_cast<float>(sf->get_index(zl,yl,xl));
+    assert(sf->at(1,2,3) == 33);
+    assert(sf->at(2,1,3) == 48);
+}
+
 int main(){
-    
-    {
-        // Scalar field with 0 buffer layers
 
-        const auto zsize = 4;
-        const auto ysize = 5;
-        const auto xsize = 7;
-        
-        // Scalar field
-        Field::Field<float,0> sf(zsize, ysize, xsize, 0.0f);
-        size_t zdim, ydim, xdim;
-        std::tie(zdim, ydim, xdim) = sf.get_dimensions();
-        assert(std::tie(zdim, ydim, xdim) == std::tie(zsize, ysize, xsize));
-        assert(sf.get_vector_length() == 1);
-        // Set data using iterator
-        auto ctr = 0;
-        for (auto it=sf.begin(); it!=sf.end(); ++it)
-            *it = ctr++;
-        // Get data using at()
-        Field::FieldExtents e = sf.get_extents();
-        assert(e.zbegin == 0); assert(e.zend == zsize);
-        assert(e.ybegin == 0); assert(e.yend == ysize);
-        assert(e.xbegin == 0); assert(e.xend == xsize);
-        for (auto zl=e.zbegin; zl<e.zend; ++zl)
-            for (auto yl=e.ybegin; yl<e.yend; ++yl)
-                for (auto xl=e.xbegin; xl<e.xend; ++xl){
-                    auto ndx3d = xl+(yl+zl*ydim)*xdim;
-                    assert(sf.at(zl,yl,xl) == ndx3d);
-                }
-    }
-    
-    {
-        // Vector field with 0 buffer layers
-
-        const auto zsize = 4;
-        const auto ysize = 5;
-        const auto xsize = 7;
-        const auto vsize = 2;
-
-        Field::Field<float,0> vf(zsize, ysize, xsize, vsize, 0.0f);
-        size_t zdim, ydim, xdim;
-        std::tie(zdim, ydim, xdim) = vf.get_dimensions();
-        assert(std::tie(zdim, ydim, xdim) == std::tie(zsize, ysize, xsize));
-        assert(vf.get_vector_length() == vsize);
-        // Set data using iterator
-        auto ctr = 0;
-        for (auto it=vf.begin(); it!=vf.end(); ++it)
-            *it = ctr++;
-
-        // Get data using at()
-        Field::FieldExtents e = vf.get_extents();
-        assert(e.zbegin == 0); assert(e.zend == zsize);
-        assert(e.ybegin == 0); assert(e.yend == ysize);
-        assert(e.xbegin == 0); assert(e.xend == xsize);
-        auto vlen = vf.get_vector_length();
-        for (auto zl=e.zbegin; zl<e.zend; ++zl)
-            for (auto yl=e.ybegin; yl<e.yend; ++yl)
-                for (auto xl=e.xbegin; xl<e.xend; ++xl){
-                    auto ndx3d = xl+(yl+zl*ydim)*xdim;
-                    for (auto kl=0; kl<vlen; ++kl)
-                        assert(vf.at(zl,yl,xl,kl) == kl+ndx3d*vlen);
-                }
-    }
-
-    {
-        const auto zsize=2;
-        const auto ysize=2;
-        const auto xsize=3;
-        
-        // Scalar field with one buffer layer
-        auto sf = std::make_unique<Field::Field<float,1> >(zsize, ysize, xsize, 0.0f);
-        size_t zdim, ydim, xdim;
-        std::tie(zdim, ydim, xdim) = sf->get_dimensions();
-        assert(zdim == zsize+2); assert(ydim == ysize+2); assert(xdim == xsize+2);
-        Field::FieldExtents e = sf->get_extents();
-        assert(e.zbegin == 1); assert(e.zend == zsize+1);
-        assert(e.ybegin == 1); assert(e.yend == ysize+1);
-        assert(e.xbegin == 1); assert(e.xend == xsize+1);
-        for (auto zl=e.zbegin; zl<e.zend; ++zl)
-            for (auto yl=e.ybegin; yl<e.yend; ++yl)
-                for (auto xl=e.xbegin; xl<e.xend; ++xl){
-                    auto ndx3d = xl+(yl+zl*ydim)*xdim;
-                    sf->at(zl,yl,xl) = static_cast<float>(ndx3d);
-                }
-        assert(sf->at(1,2,3) == 33);
-        assert(sf->at(2,1,3) == 48);
-    }
+    test_scalar_field_with_0_buffer_layers();
+    test_vector_field_with_0_buffer_layers();
+    test_scalar_field_with_1_buffer_layers();
 
     return 0;
 }
