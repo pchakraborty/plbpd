b33be6b1516e095ea3abbf2cbad90d0bdf659676
Purnendu Chakraborty <purnendu.chakraborty@nasa.gov>
2018-06-21 11:16:42 -0400

    Added const correctness

--- src/main.cpp
+++ src/main.cpp
@@ -18,19 +18,19 @@ int main(){
     // Flow definition: domain, boundary, model
     const auto flow = std::make_unique<Flow>("Poiseuille2D");
     const auto domain = flow->get_flow_domain();
-    auto boundary = flow->get_flow_boundary();
+    const auto boundary = flow->get_flow_boundary();
     const auto lbmodel = flow->get_lbmodel();
     const auto num_timesteps = flow->get_num_timesteps();
 
     // Lattice Boltzmann dynamics
-    const auto collide = CollisionSRT(lbmodel, domain);
-    const auto stream = Streaming(lbmodel, "push");
+    const auto collide = std::make_unique<CollisionSRT>(lbmodel, domain);
+    const auto stream = std::make_unique<Streaming>(lbmodel, "push");
     
     // Lattice
-    size_t kdim = lbmodel->get_num_directions();
+    const size_t kdim = lbmodel->get_num_directions();
     auto lattice = Lattice(domain->get_dimensions(), kdim);
 
-    auto calc_moments = CalcMoments();
+    const auto calc_moments = std::make_unique<CalcMoments>();
 
     // Initialize
     domain->initialize(lattice);
@@ -40,12 +40,11 @@ int main(){
     // Time loop
     tbb::tick_count start = tbb::tick_count::now();
     for (auto i=0; i<num_timesteps; ++i){
-        // std::cout<<"step: "<<i<<std::endl;
-        collide(lattice);
-        stream(lattice);
+        collide->operator()(lattice);
+        stream->operator()(lattice);
         boundary->apply_noslip(lattice);
         boundary->apply_periodicity(lattice);
-        calc_moments(lbmodel, lattice);
+        calc_moments->operator()(lbmodel, lattice);
         boundary->reset(lattice);
     }
     auto elapsed = (tbb::tick_count::now()-start).seconds();
@@ -53,9 +52,9 @@ int main(){
     lattice.write_state("FinalState.h5");
 
     // Print times
-    std::cout<<"Collide: "<<collide.get_total_time()<<"s\n";
-    std::cout<<"Stream: "<<stream.get_total_time()<<"s\n";
-    std::cout<<"CalcMoments: "<<calc_moments.get_total_time()<<"s\n";
+    std::cout<<"Collide: "<<collide->get_total_time()<<"s\n";
+    std::cout<<"Stream: "<<stream->get_total_time()<<"s\n";
+    std::cout<<"CalcMoments: "<<calc_moments->get_total_time()<<"s\n";
     std::cout<<"Boundary: "<<boundary->get_total_time()<<"s\n";
     std::cout<<"-noslip: "<<boundary->get_time_noslip()<<"s\n";
     std::cout<<"-periodicity: "<<boundary->get_time_periodicity()<<"s\n";
