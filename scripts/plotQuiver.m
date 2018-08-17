47adc38eba4029b42871f837fad5907a9a53c2d1
Purnendu Chakraborty <purnendu.chakraborty@nasa.gov>
2018-08-17 12:45:53 -0400

    Removed some old scripts and doc

--- scripts/plotQuiver.m
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/octave -qf
-% function [] = plotQuiver(filename, Lx, Lz, scale)
-function [] = plotQuiver(filename, Lx, Lz, scale)
-
-velocity = load(filename);
-ux = velocity(1:Lz,1:Lx);
-uz = velocity(2*Lz+1:3*Lz,1:Lx);
-[x, z] = meshgrid(1:1:Lx,1:1:Lz);
-quiver(x,z,ux,uz,scale);
