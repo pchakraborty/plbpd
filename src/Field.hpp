cb458abff4059088afbc52802f366c81571868b8
Purnendu Chakraborty <purnendu.chakraborty@nasa.gov>
2018-08-20 10:44:31 -0400

    Field.hpp::ScalarField is now a sub-class of VectorField

--- src/Field.hpp
+++ src/Field.hpp
@@ -24,15 +24,15 @@ class VectorField {
     uint32_t _zfactor, _yfactor, _xfactor;  // factors for index calculation
     FieldExtents _e;
 
-    void _check_arg(uint32_t arg) {
+    void _check_non_zero_arg(const uint32_t arg) {
         if (arg == 0)
             throw std::invalid_argument("argument is zero");
     }
 
-    void _set_lengths(uint32_t zlen,
-                      uint32_t ylen,
-                      uint32_t xlen,
-                      uint32_t vlen) {
+    void _set_lengths(const uint32_t zlen,
+                      const uint32_t ylen,
+                      const uint32_t xlen,
+                      const uint32_t vlen) {
         _zlen = zlen + 2*num_buffer_layers;
         _ylen = ylen + 2*num_buffer_layers;
         _xlen = xlen + 2*num_buffer_layers;
@@ -56,13 +56,13 @@ class VectorField {
 
  public:
     // Initialize a field of vectors (of length vlen)
-    VectorField(uint32_t zlen,
-                uint32_t ylen,
-                uint32_t xlen,
-                uint32_t vlen,
+    VectorField(const uint32_t zlen,
+                const uint32_t ylen,
+                const uint32_t xlen,
+                const uint32_t vlen,
                 const T& value) {
         for (auto arg: std::vector<uint32_t> {zlen, ylen, xlen, vlen})
-            _check_arg(arg);
+            _check_non_zero_arg(arg);
         _set_lengths(zlen, ylen, xlen, vlen);
         _arrdata.resize(_zlen*_ylen*_xlen*_vlen, value);
         _set_factors();
@@ -114,17 +114,11 @@ class VectorField {
 };  // class VectorField
 
 template <typename T, uint32_t num_buffer_layers>
-class ScalarField final {
-    using VectorFieldType = VectorField<T, num_buffer_layers>;
-
- private:
-    std::shared_ptr<VectorFieldType> _vf;
-
+class ScalarField : public VectorField<T, num_buffer_layers> {
  public:
-    ScalarField(uint32_t zlen, uint32_t ylen, uint32_t xlen, const T& value) {
-        // scalar is vector of length 1
-        _vf = std::make_shared<VectorFieldType>(zlen, ylen, xlen, 1, value);
-    }
+
+    ScalarField(uint32_t zlen, uint32_t ylen, uint32_t xlen, const T& value)
+        : VectorField<T, num_buffer_layers>(zlen, ylen, xlen, 1, value) {}
 
     ~ScalarField() {}
 
@@ -132,32 +126,24 @@ class ScalarField final {
 
     ScalarField& operator=(ScalarField&) = delete;
 
-    inline std::tuple<uint32_t, uint32_t, uint32_t> get_dimensions() const {
-        return _vf->get_dimensions();
-    }
-
-    inline const FieldExtents& get_extents() const {
-        return _vf->get_extents();
-    }
-
     inline uint32_t sub2ind(uint32_t z, uint32_t y, uint32_t x) {
-        return _vf->sub2ind(z, y, x, 0);
+        return VectorField<T, num_buffer_layers>::sub2ind(z, y, x, 0);
     }
 
     inline T& at(uint32_t z, uint32_t y, uint32_t x) {
-        return _vf->at(z, y, x, 0);
+        return VectorField<T, num_buffer_layers>::at(z, y, x, 0);
     }
 
     inline const T& at(uint32_t z, uint32_t y, uint32_t x) const {
-        return _vf->at(z, y, x, 0);
+        return VectorField<T, num_buffer_layers>::at(z, y, x, 0);
     }
 
     inline const T* get(uint32_t z, uint32_t y, uint32_t x) const {
-        return _vf->get(z, y, x, 0);
+        return VectorField<T, num_buffer_layers>::get(z, y, x, 0);
     }
 
     inline T* get(uint32_t z, uint32_t y, uint32_t x) {
-        return _vf->get(z, y, x, 0);
+        return VectorField<T, num_buffer_layers>::get(z, y, x, 0);
     }
 };  // class SclalarField
 
