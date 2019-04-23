331b8b56fd1b20c184cae9ee687f54535aef110e
Purnendu Chakraborty <pchakraborty@gmail.com>
2019-04-23 01:46:41 -0400

    Renamed z, y, x as x1, x2, x3 etc. Added a ctor that accepts a shape array and vlen. Related changes to utest

--- src/field/VectorField.hpp
+++ src/field/VectorField.hpp
@@ -13,22 +13,16 @@ namespace field {
 template <typename T, uint32_t n_halo_layers>
 class VectorField {
 
-    using uintArray3 = std::array<uint32_t, 3>;
-    using intArray3 = std::array<int32_t, 3>;
-
 public:
 
-    // Initialize a field of vectors (of length vsize)
-    VectorField(uint32_t zsize, uint32_t ysize, uint32_t xsize, uint32_t vlen) {
-        // _sanity_check(std::array<uint32_t, 4>{zsize, ysize, xsize, vlen};
-        _check_positive_arg(zsize);
-        _check_positive_arg(ysize);
-        _check_positive_arg(xsize);
-        _check_positive_arg(vlen);
-        _set_lengths(zsize, ysize, xsize, vlen);
-        _arrdata.resize(_zsize*_ysize*_xsize*_vsize, static_cast<T>(0));
-        _set_factors();
-        _set_extents();
+    // Initialize a field of vectors (of length vlen)
+    VectorField(std::array<uint32_t, 3> field_shape, uint32_t vlen) {
+        _initialize(field_shape, vlen);
+    }
+
+    VectorField(uint32_t shp1, uint32_t shp2, uint32_t shp3, uint32_t vlen) {
+        std::array<uint32_t, 3> field_shape{shp1, shp2, shp3};
+        _initialize(field_shape, vlen);
     }
 
     ~VectorField() {}
@@ -36,75 +30,93 @@ public:
     VectorField(VectorField&) = delete;
     VectorField& operator=(VectorField&) = delete;
 
-    inline std::tuple<uint32_t, uint32_t, uint32_t> get_field_size() const {
-        return std::make_tuple(_zsize, _ysize, _xsize);
+    inline std::array<uint32_t, 3> get_shape() const {
+        return std::array<uint32_t, 3>{_shape1, _shape2, _shape3};
     }
 
     inline uint32_t get_vector_length() const {
-        return _vsize;
+        return _vlen;
     }
 
-    inline const FieldExtent& get_field_extent() const {
+    inline const FieldExtent& get_extent() const {
         return _e;
     }
 
-    inline T& at(uint32_t z, uint32_t y, uint32_t x, uint32_t v) {
-        return _arrdata[_sub2ind(z, y, x, v)];
+    inline T& at(uint32_t x1, uint32_t x2, uint32_t x3, uint32_t v) {
+        return _arrdata[_sub2ind(x1, x2, x3, v)];
     }
 
-    inline const T& at(uint32_t z, uint32_t y, uint32_t x, uint32_t v) const {
-        return _arrdata[_sub2ind(z, y, x, v)];
+    inline const T& at(uint32_t x1, uint32_t x2, uint32_t x3, uint32_t v) const {
+        return _arrdata[_sub2ind(x1, x2, x3, v)];
     }
 
-    inline const T* get(uint32_t z, uint32_t y, uint32_t x, uint32_t v) const {
-        return &_arrdata[_sub2ind(z, y, x, v)];
+    inline const T* get(uint32_t x1, uint32_t x2, uint32_t x3, uint32_t v) const {
+        return &_arrdata[_sub2ind(x1, x2, x3, v)];
     }
 
-    inline T* get(uint32_t z, uint32_t y, uint32_t x, uint32_t v) {
-        return &_arrdata[_sub2ind(z, y, x, v)];
+    inline T* get(uint32_t x1, uint32_t x2, uint32_t x3, uint32_t v) {
+        return &_arrdata[_sub2ind(x1, x2, x3, v)];
     }
 
-    inline intArray3
-    get_neighbor(uint32_t z, uint32_t y, uint32_t x,  const intArray3 & ck) const {
-        return intArray3{z+ck[2], y+ck[1], x+ck[0]};
+    inline std::array<int32_t, 3> get_neighbor(
+        uint32_t x1, uint32_t x2, uint32_t x3,
+        const std::array<int32_t, 3> & ck) const
+    {
+        return std::array<int32_t, 3>{x1+ck[2], x2+ck[1], x3+ck[0]};
     }
 
 private:
 
-    uint32_t _zsize, _ysize, _xsize;  // field size in each direction
-    uint32_t _vsize;  // vector length at each field location
+    uint32_t _shape1, _shape2, _shape3;  // field size in each direction
+    uint32_t _vlen;  // vector length at each field location
     std::vector<T> _arrdata;
     FieldExtent _e;
-    uint32_t _zfactor, _yfactor, _xfactor;  // factors for index calculation
+    uint32_t _factor1, _factor2, _factor3;  // factors for index calculation
+
+    void _initialize(std::array<uint32_t, 3>& field_shape, uint32_t vlen) {
+        _set_lengths(field_shape, vlen);
+        _create_internal_array();
+        _set_factors();
+        _set_extents();
+    }        
 
     void _check_positive_arg(const uint32_t arg) {
         if (arg < 1)  throw std::invalid_argument("argument is less than 1");
     }
 
-    void _set_lengths(uint32_t zsize, uint32_t ysize, uint32_t xsize, uint32_t vsize) {
-        _zsize = zsize + 2*n_halo_layers;
-        _ysize = ysize + 2*n_halo_layers;
-        _xsize = xsize + 2*n_halo_layers;
-        _vsize = vsize;
+    void _set_lengths(std::array<uint32_t, 3>& shape, uint32_t vlen) {
+        for (auto& element: shape)
+            if (element < 1)
+                throw std::invalid_argument("Field shape cannot be < 1");
+        _shape1 = shape[0] + 2*n_halo_layers;
+        _shape2 = shape[1] + 2*n_halo_layers;
+        _shape3 = shape[2] + 2*n_halo_layers;
+        if (vlen < 0)
+            throw std::invalid_argument("Vector length cannot be < 0");
+        _vlen = vlen;
+    }
+
+    void _create_internal_array() {
+        _arrdata.resize(_shape1*_shape2*_shape3*_vlen, static_cast<T>(0));
     }
 
     void _set_factors() {
-        _zfactor = _vsize*_xsize*_ysize;
-        _yfactor = _vsize*_xsize;
-        _xfactor = _vsize;
+        _factor1 = _vlen*_shape3*_shape2;
+        _factor2 = _vlen*_shape3;
+        _factor3 = _vlen;
     }
 
     void _set_extents() {
         _e.zbegin = 0 + n_halo_layers;
-        _e.zend = _zsize - n_halo_layers;
+        _e.zend = _shape1 - n_halo_layers;
         _e.ybegin = 0 + n_halo_layers;
-        _e.yend = _ysize - n_halo_layers;
+        _e.yend = _shape2 - n_halo_layers;
         _e.xbegin = 0 + n_halo_layers;
-        _e.xend = _xsize - n_halo_layers;
+        _e.xend = _shape3 - n_halo_layers;
     }
 
     inline uint32_t _sub2ind(uint32_t z, uint32_t y, uint32_t x, uint32_t v) {
-        return z*_zfactor + y*_yfactor + x*_xfactor + v;
+        return z*_factor1 + y*_factor2 + x*_factor3 + v;
     }
 
 
--- src/field/utests/TestVectorField.cpp
+++ src/field/utests/TestVectorField.cpp
@@ -1,29 +1,30 @@
 #include <cassert>
 #include <tuple>
+#include <iostream>
 
 #include "../VectorField.hpp"
 
 template<typename T, uint32_t n_halo_layers>
 void assert_extents(field::VectorField<T, n_halo_layers>& vf) {
-    field::FieldExtent e = vf.get_field_extent();
-    uint32_t zsize, ysize, xsize;
-    std::tie(zsize, ysize, xsize) = vf.get_field_size();
+    field::FieldExtent e = vf.get_extent();
+    std::array<uint32_t, 3> shape = vf.get_shape();
     assert(e.zbegin == 0 + n_halo_layers);
-    assert(e.zend == zsize + n_halo_layers);
+    assert(e.zend == shape[0] + n_halo_layers);
     assert(e.ybegin == 0 + n_halo_layers);
-    assert(e.yend == ysize + n_halo_layers);
+    assert(e.yend == shape[1] + n_halo_layers);
     assert(e.xbegin == 0 + n_halo_layers);
-    assert(e.xend == xsize + n_halo_layers);
+    assert(e.xend == shape[2] + n_halo_layers);
 }
 
 void test_vectorfield_with_0_halo_layers() {
-    const uint32_t zsize = 4, ysize = 5, xsize = 3; uint32_t vsize = 4;
+    const std::array<uint32_t, 3>shape{4, 5, 3};
+    const uint32_t vlen = 2;
     const uint32_t n_halo_layers = 0;
     
-    field::VectorField<float, n_halo_layers> vf(zsize, ysize, xsize, vsize);
+    field::VectorField<float, n_halo_layers> vf(shape[0], shape[1], shape[2], vlen);
     
-    assert(std::make_tuple(zsize, ysize, xsize) == vf.get_field_size());
-    assert(vsize == vf.get_vector_length());
+    assert(shape == vf.get_shape());
+    assert(vlen == vf.get_vector_length());
     assert_extents<float, n_halo_layers>(vf);    
 }
 
